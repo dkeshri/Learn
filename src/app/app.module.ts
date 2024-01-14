38200bdf3c3b36c0f9733dd31b7762b8409401dc
dkeshri <dkeshridev@gmail.com>
2024-01-14 16:59:47 +0530

    Added core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core/core/core.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +12,8 @@ import { SharedModule } from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    CoreModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
