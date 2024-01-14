374fd58265b4a337516837ce7c3b3a021fd5130f
dkeshri <dkeshridev@gmail.com>
2024-01-14 16:56:05 +0530

    module added

--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
 import { ViewsRoutingModule } from './views-routing.module';
 import { SharedModule } from '../shared/shared.module';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent
+  ],
   imports: [
     SharedModule,
     ViewsRoutingModule,
