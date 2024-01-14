21fe4bdb3a938d6a9eef1d8339e45ab49b9ec29e
dkeshri <dkeshridev@gmail.com>
2024-01-14 14:29:49 +0530

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'views', loadChildren:()=>import('./views/views.module').then(module=>module.ViewsModule)
+    },
+    {
+        path:'', redirectTo:'views',pathMatch:'full'
+    },
+    {
+    path:'**',
+    component:PageNotFoundComponent
+    }
+];
