39d8c55a696c2f60f661faa210ff76a98da0a14b
dkeshri <dkeshridev@gmail.com>
2024-01-15 19:07:36 +0530

    youtube video

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '../theme/theme.module';
+import { VideoCardComponent } from './components/video-card/video-card.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    VideoCardComponent
+  ],
   imports: [
     ThemeModule
   ],
   exports: [
-    ThemeModule
+    ThemeModule,
+    VideoCardComponent
   ]
 })
 export class SharedModule { }
