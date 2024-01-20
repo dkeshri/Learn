3605d5e1caa749dfa9bdea5f668f6cfd6441216d
dkeshri <dkeshridev@gmail.com>
2024-01-20 12:27:40 +0530

    intercepter spec

--- src/app/core/interceptors/http-request.interceptor.spec.ts
+++ src/app/core/interceptors/http-request.interceptor.spec.ts
@@ -1,17 +1,17 @@
 import { TestBed } from '@angular/core/testing';
 import { HttpInterceptorFn } from '@angular/common/http';
 
-import { httpRequestInterceptor } from './http-request.interceptor';
+import { HttpRequestInterceptor } from './http-request.interceptor';
 
-describe('httpRequestInterceptor', () => {
-  const interceptor: HttpInterceptorFn = (req, next) => 
-    TestBed.runInInjectionContext(() => httpRequestInterceptor(req, next));
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-  });
+describe('HttpRequestInterceptor', () => {
+  beforeEach(() => TestBed.configureTestingModule({
+    providers: [
+      HttpRequestInterceptor
+      ]
+  }));
 
   it('should be created', () => {
+    const interceptor: HttpRequestInterceptor = TestBed.inject(HttpRequestInterceptor);
     expect(interceptor).toBeTruthy();
   });
 });
