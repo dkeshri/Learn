ffc3905636a1ab59cee63233de1568a184cae090
dkeshri <dkeshridev@gmail.com>
2024-01-20 13:28:15 +0530

    Add topic component

--- src/app/views/add-topic/add-topic.component.ts
+++ src/app/views/add-topic/add-topic.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
 
@@ -8,7 +9,23 @@ import { DynamicDialogRef } from 'primeng/dynamicdialog';
   styleUrl: './add-topic.component.css'
 })
 export class AddTopicComponent {
-  constructor(private dialogRef: DynamicDialogRef,
+  unitForm: FormGroup = this.formBuilder.group({
+    title: ['', [Validators.required]],
+    url: ['',[Validators.required]],
+    token:['',[Validators.required]]
+  });
+  get topicTitle() {
+    return this.unitForm.get('title') as FormControl;
+  }
+  get topicURL() {
+    return this.unitForm.get('url') as FormControl;
+  }
+  get topicToken() {
+    return this.unitForm.get('token') as FormControl;
+  }
+  constructor(
+    private formBuilder: FormBuilder,
+    private dialogRef: DynamicDialogRef,
     private messageService: MessageService,){
     
   }
