777b5d22d875e09774cf4eca9764a279768ef214
dkeshri <dkeshridev@gmail.com>
2024-01-20 13:37:08 +0530

    taje data

--- src/app/views/add-topic/add-topic.component.ts
+++ src/app/views/add-topic/add-topic.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { MessageService } from 'primeng/api';
 import { DynamicDialogRef } from 'primeng/dynamicdialog';
+import { Topic } from '../Models/topic-model';
 
 @Component({
   selector: 'app-add-topic',
@@ -9,19 +10,19 @@ import { DynamicDialogRef } from 'primeng/dynamicdialog';
   styleUrl: './add-topic.component.css'
 })
 export class AddTopicComponent {
-  unitForm: FormGroup = this.formBuilder.group({
+  topicForm: FormGroup = this.formBuilder.group({
     title: ['', [Validators.required]],
     url: ['',[Validators.required]],
     token:['',[Validators.required]]
   });
   get topicTitle() {
-    return this.unitForm.get('title') as FormControl;
+    return this.topicForm.get('title') as FormControl;
   }
   get topicURL() {
-    return this.unitForm.get('url') as FormControl;
+    return this.topicForm.get('url') as FormControl;
   }
   get topicToken() {
-    return this.unitForm.get('token') as FormControl;
+    return this.topicForm.get('token') as FormControl;
   }
   constructor(
     private formBuilder: FormBuilder,
@@ -30,7 +31,13 @@ export class AddTopicComponent {
     
   }
   onSave(){
-    // when we want to send some data to parent coomponent. here not required
+    let topic = {} as Topic;
+    topic.title = this.topicForm.controls['title'].value;
+    topic.url = this.topicForm.controls['url'].value;
+    topic.token = this.topicForm.controls['token'].value;
+
+    // here we will call github lib.
+        // when we want to send some data to parent coomponent. here not required
     this.dialogRef?.close(1);
     this.messageService.add({ severity: 'success', summary: 'Topic Added Successfully', detail: 'Topic Titile', life: 3000 });
   }
